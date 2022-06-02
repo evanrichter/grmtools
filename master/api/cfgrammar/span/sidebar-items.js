6aeb9a2af83e0e5bb9b556ed669198d1dd5d74a0
github_hooks <noreply@soft-dev.org>
2022-06-02 09:54:13 +0100

    Deploy gh-pages

---
+++ master/api/cfgrammar/span/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Span","A `Span` records what portion of the user’s input something (e.g. a lexeme or production) references (i.e. the `Span` doesn’t hold a reference / copy of the actual input)."]]});
